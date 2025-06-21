5340954ce1693352b8b2e882f7adc8de1e03dba2
Qasim <qasimzexal@gmail.com>
2025-06-21 13:42:25 +0500

    removed table of contents.

--- src/site.config.ts
+++ src/site.config.ts
@@ -47,7 +47,7 @@ export const theme: ThemeUserConfig = {
   /** Configure the header of your site. */
   header: {
     menu: [
-      { title: 'Blog', link: '/blog' },
+    
       { title: 'Projects', link: '/projects' },
       { title: 'About', link: '/about' }
     ]
