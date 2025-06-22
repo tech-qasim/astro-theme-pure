091d1e83963e3c86553e68c03a85920b21d438c2
Qasim <qasimzexal@gmail.com>
2025-06-22 13:03:48 +0500

    Full-Stack Mobile developer

--- src/site.config.ts
+++ src/site.config.ts
@@ -7,7 +7,7 @@ export const theme: ThemeUserConfig = {
   /** Will be used in index page & copyright declaration */
   author: 'Mohammad Qasim', 
   /** Description metadata for your website. Can be used in page metadata. */
-  description: 'Full-Stack Flutter developer',
+  description: 'Full-Stack Mobile developer',
   /** The default favicon for your site which should be a path to an image in the `public/` directory. */
   favicon: '/favicon/favicon.ico',
   /** Specify the default language for this site. */
